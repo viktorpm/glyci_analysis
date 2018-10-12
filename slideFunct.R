9d4f55bc9425252e23d98aa0282ac7eec9787a58
Viktor Plattner <pviktor@THALAMUS130627.koki.local>
2018-10-12 17:56:57 +0200

    moving average and moving sd (function and plot)

---
+++ slideFunct.R
@@ -0,0 +1,26 @@
+slideFunct <- function(data, window, step, type) {
+  if (missing(type)) {
+    type <- "mean"
+  }
+
+  total <- length(data)
+  spots <- seq(from = 1, to = (total - window), by = step)
+  result <- vector(length = length(spots))
+
+  ### calculate sd
+  if (type == "sd") {
+    for (i in 1:length(spots)) {
+      result[i] <- sd(data[spots[i]:(spots[i] + window)])
+    }
+    return(result)
+  }
+  
+  ### 
+  if (type == "mean") {
+    for(i in 1:length(spots)){
+      result[i] <- mean(data[spots[i]:(spots[i]+window)])
+    }
+    return(result)
+  }
+  
+}
--- spont_desynchron_analysis.R
+++ spont_desynchron_analysis.R
@@ -2,7 +2,7 @@ library(R.matlab)
 library(tidyverse)
 
 
-raw.rec <- readMat(file.path("x:","extra32","_Projects","Glyci_II","DATA","ELPHYS","juxta","GII-21","01_right_PnO_t1_4058_baseline1_felebredos.mat"))
+raw.rec <- readMat(file.path("x:","extra32","_Projects","Glyci_II","DATA","ELPHYS","juxta","GII-25","08_right_PnO_t5_4083_baseline1_wake.mat"))
 
 
 raw.rec$ap[,,1]$interval
@@ -24,10 +24,10 @@ raw.rec$ap[,,1]$interval
 
 
 which(raw.rec$ap[,,1]$values[150,] == max(raw.rec$ap[,,1]$values[150,]))
-raw.rec$ap[,,1]$interval*11*1000 ### time of the peak of the APs after the first point 
+raw.rec$ap[,,1]$interval*6*1000 ### time of the peak of the APs after the first point 
 
 ap <- raw.rec$ap[,,1]$times %>% as.double()
-ap_peaks <- ap + (raw.rec$ap[,,1]$interval*11)
+ap_peaks <- ap + (raw.rec$ap[,,1]$interval*6)
 ap_peaks <- ap_peaks %>% as.tibble()
 
 ### EEG ----------------------------------------------------------
@@ -48,7 +48,7 @@ rec_length <- (raw.rec$EEG[,,1]$length/samp_rate) %>% as.double()
 EEG_scaled <- (EEG*as.double(raw.rec$EEG[,,1]$scale)) + as.double(raw.rec$EEG[,,1]$offset)
 
 ### EEG_ds (downsampled) --------------------------------------------------------
-EEG_ds <- raw.rec$eeg.ds[,,1]$values
+EEG_ds <- raw.rec$eeg.ds[,,1]$values %>% as.double()
 raw.rec$eeg.ds[,,1]$length/40
 
 samp_rate_ds <- 1000/(raw.rec$eeg.ds[,,1]$interval*1000) %>% as.double()
@@ -68,14 +68,48 @@ plot(EEG_ds_scaled[plot_from:plot_to], type = "l")
 ### times of data points
 times <- seq(from = 0, to = rec_length-interval_ds, by = interval_ds)
 
+### calculate moving sd
+source("slideFunct.R")
+slide_sd <- slideFunct(data = EEG_ds_scaled, window = 40, step = 20, type = "sd")
+slide_mean <- slideFunct(data = EEG_ds_scaled, window = 40, step = 20, type = "mean")
+
+### No. datapoints the sd/mean was calculated from
+(length(EEG_ds_scaled) / length(slide_sd)) %>% round()
+slide_sd_rep <- rep(slide_sd, each = (length(EEG_ds_scaled) / length(slide_sd)) %>% round())
+
+slide_mean_rep <- rep(slide_mean, each = (length(EEG_ds_scaled) / length(slide_mean)) %>% round())
+
+
+### length difference of the vectors
+length(EEG_ds_scaled) - length(slide_sd_rep)
+
+
+### filling length difference
+slide_sd_rep <- c(slide_sd_rep, rep(slide_sd_rep[length(slide_sd_rep)], 
+                                    length(EEG_ds_scaled) - length(slide_sd_rep))
+                  )
+slide_mean_rep <- c(slide_mean_rep, rep(slide_mean_rep[length(slide_mean_rep)], 
+                                     length(EEG_ds_scaled) - length(slide_mean_rep))
+)
+
+
 EEG_ds_df <- EEG_ds_scaled %>% 
   ts(start = 0, 
      end = rec_length-interval_ds, 
      deltat = 1/samp_rate_ds) %>% 
-  data.frame() %>% 
-  mutate(times) #%>% 
-  # rename(Series.1 = ".")
+  as.data.frame() %>% 
+  mutate(times) %>%
+  mutate(sd = slide_sd_rep) %>% 
+  mutate(mean = slide_mean_rep)
 
+ggplot(data = EEG_ds_df, mapping = aes(x = times, y = x)) +
+  geom_line() + 
+  xlim(100,200) + 
+  geom_line(data = EEG_ds_df, mapping = aes(x = times, y = sd), color = "red") +
+  geom_line(data = EEG_ds_df, mapping = aes(x = times, y = mean), color = "blue")
+
+
+  
 identical(
 EEG_ds_df %>% 
   dplyr::slice(plot_from:plot_to),
@@ -92,6 +126,8 @@ EEG_ds_df %>%
 # tmp_df <- data.frame(EEG_scaled %>% ts())
 # tmps_small <- tmp_df %>% 
 #   slice(1:100000)
+
+### Wavelet -------------------------------------------------------------------
 
 library(WaveletComp)
 wave <- analyze.wavelet(EEG_ds_df %>% 
@@ -123,12 +159,6 @@ powers %>% dim()
 freqs <- (1/wave$Period) %>% round(2)
 period <- (wave$Period) %>%  round(2)
 
-#rownames(powers) <- freqs 
-
-# powers <- powers %>% 
-#   as.tibble() %>% 
-#   mutate(scale = freqs)
-
 
 library(reshape2)
 library(ggplot2)
@@ -148,7 +178,6 @@ range(freqs)
 range(period)
 
 
-
 freqs %>% plot()
 -log1p(powers$Var1[1:162]) %>% plot(col = "red")
 
@@ -158,14 +187,21 @@ freqs %>% plot()
 ((powers$Var1[1:162] / freqs)/100) %>% 
   rev()
 
-### Plotting --------------------------------------------------------------
+### Plotting wavelet --------------------------------------------------------------
 ggplot() + 
   theme_minimal() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.text.y = element_text(margin = margin(t = 0, r = -30, b = 0, l = 20)),
+        axis.text.x = element_text(margin = margin(t = -20, r = 0, b = 20, l = 0)),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks.length = unit(5, "mm")) +
   geom_raster(data = powers,
               mapping = aes(x = Var2/samp_rate_ds + 250,
                             y = Var1,
-                            fill = value)) +
-  xlab("Time") +
+                            fill = scale(value))) +
+  xlab("Time (s)") +
   scale_y_reverse(name = "Frequency (Hz)",
                      breaks = c(1,25,50,75,100,125,150),
                      labels = c(freqs[1],
@@ -176,15 +212,15 @@ ggplot() +
                                 freqs[125],
                                 freqs[150])) +
   #scale_y_reverse() +
-  scale_fill_distiller(palette = "RdGy") +
+  scale_fill_distiller(palette = "RdGy", name = "Power") +
   geom_line(data = EEG_ds_df %>% 
               dplyr::slice(plot_from:plot_to), 
-            mapping = aes(x = times, y = -Series.1+20), color = "white") + 
+            mapping = aes(x = times, y = -2*Series.1+20), color = "white") + 
   geom_point(data = ap_peaks %>% 
                 filter(value > 250.1, value < 290), 
              mapping = aes(x = value, y = 100),
              shape = "|",
-             color = "white") 
+             color = "white", size = 4) 
 
   
 
