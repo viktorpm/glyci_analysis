cbe34386070ea453ba1a382bf6a63ceda082f46a
Viktor Plattner <pviktor@THALAMUS130627.koki.local>
2018-10-16 17:38:51 +0200

    filtering EEG, data frames from ISI and AC data, ggplotting result

--- spont_desynchron_analysis.R
+++ spont_desynchron_analysis.R
@@ -1,8 +1,8 @@
 library(R.matlab)
 library(tidyverse)
+load(file.path("f:","_R_WD","useful_to_load","colorMatrix.RData"))
 
-
-raw.rec <- readMat(file.path("data","07_4294_gv03.mat"))
+raw.rec <- readMat(file.path("data","04_t1_4316_baseline3_gv121.mat"))
 
 raw.rec$ap[,,1]$interval
 ### contents of ap channel  
@@ -21,10 +21,12 @@ raw.rec$ap[,,1]$interval
   ### codes: marker codes of the APs
   ### values: matrix containing the waveforms of the APs (described by a given No. points)
 
-points_to_peak <- which(raw.rec$ap[,,1]$values[150,] == max(raw.rec$ap[,,1]$values[150,]))
-raw.rec$ap[,,1]$interval*points_to_peak*1000 ### time of the peak of the APs after the first point 
+points_to_peak <- which(raw.rec$ap[,,1]$values[150,] == max(raw.rec$ap[,,1]$values[150,])) %>% 
+  as.numeric()
+raw.rec$ap[,,1]$interval*points_to_peak ### time of the peak of the APs after the first point 
+
 ap <- raw.rec$ap[,,1]$times %>% as.double()
-ap_peaks <- tibble(peak_times = ap + c(raw.rec$ap[,,1]$interval*points_to_peak))
+ap_peaks <- tibble(peak_times = (ap + c(raw.rec$ap[,,1]$interval*points_to_peak)))
 
 
 
@@ -51,11 +53,17 @@ EEG_scaled <- (EEG*as.double(raw.rec$EEG[,,1]$scale)) + as.double(raw.rec$EEG[,,
 ### downsampling in R
 source(file.path("downSamp.R"))
 EEG_ds_scaled <- downSamp(data = EEG_scaled, ds_factor = 512, samp_rate = 20000)
-samp_rate_ds <- 5978/153.0243
-rec_length <- 153.0243
+samp_rate_ds <- 14340/367.0889
+rec_length <- 367.0889
 interval_ds <- 1/samp_rate_ds
 
+### Filtering 
 
+ButterHP = butter(n = 3, 
+                  W = c(1/(samp_rate_ds/2), 10/(samp_rate_ds/2) ),
+                  type = 'pass',
+                  plane = "z") # n:order, W:freq (digital filters: between 0 and 1, 1 is the Nyquist freq. eg: 20/10000 from 20 Hz)
+EEG_ds_scaled <- signal::filter(ButterHP,EEG_ds_scaled) %>% as.double()
 
 
 
@@ -125,22 +133,21 @@ EEG_ds_df <- as.matrix(EEG_ds_scaled) %>% ### csak akkor jÃ³ a waveletnek, ha mÃ
   mutate(sd = slide_sd_rep) %>% 
   mutate(mean = slide_mean_rep) %>% 
   mutate(levels = 1) %>% 
-  mutate(levels = replace(levels, sd < 0.5, 0))
+  mutate(levels = replace(levels, sd < 0.5, 0)) %>% 
+  mutate(ID = "gv120")
 
 ### plot eeg with sync desync periods
 ggplot(data = EEG_ds_df, mapping = aes(x = times, y = eeg_values)) +
   geom_line() + 
-  xlim(0,50) + 
+  xlim(100,150) + 
   geom_line(data = EEG_ds_df, mapping = aes(x = times, y = sd), color = "red") +
   geom_line(data = EEG_ds_df, mapping = aes(x = times, y = mean), color = "blue") +
   geom_line(data = EEG_ds_df, mapping = aes(x = times, y = levels+5), color = "purple") +
   geom_point(data = ap_peaks %>% 
-               filter(peak_times > 0, peak_times < 150), 
+               dplyr::filter(peak_times > 100, peak_times < 150), 
              mapping = aes(x = peak_times, y = 7),
              shape = "|",
              color = "black", size = 4)
-
-
 
 
 
@@ -148,22 +155,23 @@ ggplot(data = EEG_ds_df, mapping = aes(x = times, y = eeg_values)) +
 
 ### every first (first 0s, first 1s in levels)
 lag <- EEG_ds_df %>% 
-  filter(EEG_ds_df$levels != dplyr::lag(EEG_ds_df$levels)) %>% 
+  dplyr::filter(EEG_ds_df$levels != dplyr::lag(EEG_ds_df$levels)) %>% 
   select(times,levels) %>% 
   as.vector()
   
 ### every last (last 0s, last 1s in levels)
 lead <- EEG_ds_df %>% 
-  filter(EEG_ds_df$levels != dplyr::lead(EEG_ds_df$levels)) %>% 
+  dplyr::filter(EEG_ds_df$levels != dplyr::lead(EEG_ds_df$levels)) %>% 
   select(times,levels) %>% 
   as.vector()
 
 
+
 eeg_periods <- tibble(
-  desync_start = lag$times[seq(1,41,2)],
-  desync_end = lead$times[seq(2,42,2)],
-  sync_start = c(0,lag$times[seq(2,40,2)]),
-  sync_end = lead$times[seq(1,41,2)]
+  desync_start = lag$times[seq(1,length(lag$levels)+1,2)],
+  desync_end = lead$times[seq(2,length(lag$levels)+2,2)],
+  sync_start = c(0,lag$times[seq(2,length(lag$levels),2)]),
+  sync_end = lead$times[seq(1,length(lag$levels)+1,2)]
 )
 
 
@@ -199,31 +207,58 @@ eeg_periods <- tibble(
 
 ### list of ap times during sync and desync eeg periods
 sync_ap <- list()
-for (i in 1:21){
+for (i in 1:length(lag$levels)/2+1){
   sync_ap[[i]] = ap_peaks$peak_times[ap_peaks$peak_times > eeg_periods$sync_start[i] & 
                                   ap_peaks$peak_times < eeg_periods$sync_end[i]]
 }
 
 desync_ap <- list()
-for (i in 1:21){
+for (i in 1:length(lag$levels)/2+1){
   desync_ap[[i]] = ap_peaks$peak_times[ap_peaks$peak_times > eeg_periods$desync_start[i] & 
                                          ap_peaks$peak_times < eeg_periods$desync_end[i]]
 }
 
 ### calculating and plotting inter spike intervals during sync and desync eeg periods
-desync_isi <- lapply(desync_ap, diff) 
-sync_isi <- lapply(sync_ap, diff) 
+desync_isi <- lapply(desync_ap, diff) %>% 
+  unlist() %>% 
+  as.data.frame() %>% 
+  rename(desync_ISI = ".") %>% 
+  melt()
+  
+sync_isi <- lapply(sync_ap, diff) %>% 
+  unlist() %>% 
+  as.data.frame() %>% 
+  rename(sync_ISI = ".") %>% 
+  melt()
 
-hist(unlist(sync_isi), breaks = 500,
-     xlim = c (0,1),
-     ylim = c(0,50)
-)
+ISI <- rbind(sync_isi,desync_isi) %>% ### DOES NOT WORK WITH bind_rows 
+  rename(time = value, eeg_state = variable) %>% 
+  mutate(ID = "gv25")
 
-hist(unlist(desync_isi), breaks = 500,
-     xlim = c (0,1),
-     border = rgb(1,0,0,0.3),
-     add = T
-)
+ggplot(data = ISI, mapping = aes(x = time, fill = eeg_state)) +
+  geom_histogram(bins = 150) +
+  scale_fill_brewer(type = "div", palette = "Paired")
+
+  
+
+# ggplot(data = as.data.frame(unlist(sync_isi)), mapping = aes(x = unlist(sync_isi))) +
+#   geom_histogram(bins = 100, fill = "#1D4871") +
+#   geom_histogram(data = as.data.frame(unlist(desync_isi)), 
+#                  mapping = aes(x = unlist(desync_isi)),
+#                  fill = "#EB8104",
+#                  alpha = .3,
+#                  bins = 100)
+# 
+# hist(unlist(sync_isi), breaks = 500,
+#      xlim = c (0,1),
+#      ylim = c(0,50)
+# )
+# 
+# hist(unlist(desync_isi), breaks = 500,
+#      xlim = c (0,1),
+#      border = rgb(1,0,0,0.3),
+#      add = T
+# )
 
 
 ### calculating and plotting autocorrelation during sync and desync eeg periods 
@@ -241,19 +276,59 @@ RTM_creator <- function(x) {
 }
 
 ### calculating rel_time matrices
-sync_AC <- lapply(sync_ap, RTM_creator)
-desync_AC <- lapply(desync_ap, RTM_creator)
 
-par(mfrow = c(1,2))
-hist(unlist(sync_AC)[unlist(sync_AC) > -1 & 
-                       unlist(sync_AC) < 1], 
-     breaks = 200, 
-     ylim = c(0,800))
-hist(unlist(desync_AC)[unlist(desync_AC) > -1 & 
-                         unlist(desync_AC) < 1], 
-     breaks = 200, 
-     ylim = c(0,800))
-par(mfrow = c(1,1))
+library(reshape2)
+sync_AC <- lapply(sync_ap, RTM_creator) %>% 
+  unlist() %>% 
+  as.data.frame() %>% 
+  rename(sync_AC = ".") %>% 
+  melt()
+  
+
+desync_AC <- lapply(desync_ap, RTM_creator) %>% 
+  unlist() %>% 
+  as.data.frame() %>% 
+  rename(desync_AC = ".") %>%   
+  melt()
+
+AC <- rbind(sync_AC,desync_AC) %>% ### DOES NOT WORK WITH bind_rows 
+  rename(time = value, eeg_state = variable) %>% 
+  mutate(ID = "gv03")
+
+
+
+ggplot(data = AC %>% 
+         dplyr::filter(time > -1, time < 1), 
+       mapping = aes(x = time, fill = eeg_state)) +
+  geom_histogram(bins = 50) +
+  scale_fill_brewer(type = "div", palette = "Paired")
+  
+
+
+# ggplot(data = sync_AC %>% 
+#          filter(sync_AC > -1, sync_AC < 1), 
+#        mapping = aes(x = sync_AC)) +
+#   geom_histogram(bins = 500, 
+#                  fill = "#1D4871") +
+#   geom_histogram(data = desync_AC %>% 
+#                    filter(desync_AC > -1, desync_AC < 1), 
+#                  mapping = aes(x = desync_AC),
+#                  bins = 500,
+#                  alpha = .4,
+#                  fill = "#EB8104")
+
+
+
+# par(mfrow = c(1,2))
+# hist(unlist(sync_AC)[unlist(sync_AC) > -1 & 
+#                        unlist(sync_AC) < 1], 
+#      breaks = 200, 
+#      ylim = c(0,800))
+# hist(unlist(desync_AC)[unlist(desync_AC) > -1 & 
+#                          unlist(desync_AC) < 1], 
+#      breaks = 200, 
+#      ylim = c(0,800))
+# par(mfrow = c(1,1))
 
 
 
@@ -269,8 +344,11 @@ par(mfrow = c(1,1))
 ### Wavelet -------------------------------------------------------------------
 
 library(WaveletComp)
+
+time_window = c(50,100)
 wave <- analyze.wavelet(EEG_ds_df %>% 
-                          dplyr::filter(times > 250, times < 290), "eeg_values", 
+                          dplyr::filter(times > time_window[1], times < time_window[2]), 
+                        "eeg_values", 
                         loess.span = 0,
                         dt = 1/40,
                         ###1/sampling rate (number of intervals/time unit)  
@@ -284,15 +362,6 @@ wave <- analyze.wavelet(EEG_ds_df %>%
 ### calculate frequency from period
 #wave$Freq = c(seq(1,1, length.out = length(wave$Period)))/wave$Period
 
-
-wt.image(wave, color.key = "interval", n.levels = 150,
-         legend.params = list(lab = "wavelet power levels", mar = 4.7),
-         plot.coi = F,
-         plot.contour = F,
-         plot.ridge = F
-)
-
-
 powers <- wave$Power
 powers %>% dim()
 freqs <- (1/wave$Period) %>% round(2)
@@ -304,29 +373,20 @@ library(ggplot2)
 
 powers <- powers %>% 
   melt() #%>% 
-  #mutate(freq = rep(freqs, 801))
-
-#powers <- as.tibble(powers)
-#p <- gather(as.tibble(powers), key = "col", value = "power", scale)
-
-powers$value %>% length()
-
-powers$Var1[1:142] %>% range()
-
-range(freqs)
-range(period)
+#mutate(freq = rep(freqs, 801))
 
 
-freqs %>% plot()
--log1p(powers$Var1[1:162]) %>% plot(col = "red")
+wt.image(wave, color.key = "interval", n.levels = 150,
+         legend.params = list(lab = "wavelet power levels", mar = 4.7),
+         plot.coi = F,
+         plot.contour = F,
+         plot.ridge = F
+)
 
-((powers$Var1[1:162] / freqs)/100) %>% 
-  rev() %>% 
-  points(col = "red")
-((powers$Var1[1:162] / freqs)/100) %>% 
-  rev()
 
 ### Plotting wavelet --------------------------------------------------------------
+
+
 ggplot() + 
   theme_minimal() +
   theme(panel.grid.major = element_blank(),
@@ -337,7 +397,7 @@ ggplot() +
         panel.border = element_blank(),
         axis.ticks.length = unit(5, "mm")) +
   geom_raster(data = powers,
-              mapping = aes(x = Var2/samp_rate_ds + 250,
+              mapping = aes(x = Var2/samp_rate_ds + time_window[1],
                             y = Var1,
                             fill = scale(value))) +
   xlab("Time (s)") +
@@ -353,11 +413,11 @@ ggplot() +
   #scale_y_reverse() +
   scale_fill_distiller(palette = "RdGy", name = "Power") +
   geom_line(data = EEG_ds_df %>% 
-              dplyr::slice(plot_from:plot_to), 
+              dplyr::filter(times > time_window[1], times < time_window[2]), 
             mapping = aes(x = times, y = -2*eeg_values+20), color = "white") + 
   geom_point(data = ap_peaks %>% 
-                filter(value > 250.1, value < 290), 
-             mapping = aes(x = value, y = 100),
+                dplyr::filter(peak_times > time_window[1], peak_times < time_window[2]), 
+             mapping = aes(x = peak_times, y = 100),
              shape = "|",
              color = "white", size = 4)
   # geom_line(data = EEG_ds_df %>% 
@@ -365,66 +425,3 @@ ggplot() +
   #           mapping = aes(x = times, y = -3*levels+5), color = "white") 
   
 
-  
-
-
-
-powers$Var1 %>% length()  /freqs %>% length()
-
-
-rep(freqs, 1599) %>% length()
-
-
-#scale_y_continuous(name = "Frequency (Hz)", breaks = freqs)
-#ylim(min(freqs),max(freqs))
-
-
-
-ggplot(data = EEG_ds_df %>% 
-         dplyr::slice(plot_from:plot_to), mapping = aes(x = times, y = Series.1))+
-geom_line()
-  
-
-
-
-
-
-
-
-  
-  
-
-  
-  
-  
-  
-  
-  
-  
-  
-mean.power = matrix(0,1,801)
-
-for (i in 1:length(wave$Power[1,])){
-  mean.power[1,i] = mean(wave$Power[10:20,i])
-}
-
-plot(mean.power[1,], type = 'l')
-plot(small[1], col = 'red')
-
-### No. of periods x No. of time points 
-library(colorRamps)
-which(wave$axis.2 < 0.4 & wave$axis.2 > 0.25)
-heatmap(wave$Power[151:153,1:5000], Rowv = NA, Colv = NA, col = matlab.like(256))
-
-
-heatmap(wave$Power, Rowv = NA, Colv = NA, col = matlab.like(256))
-
-wavelet.cols = matlab.like(7)
-
-library(plotly)
-plot_ly(z = wave$Power, colors = wavelet.cols, type = 'heatmap')
-
-
-
-
-  
