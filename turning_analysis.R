53b9c759e9a0b18ebd14d28ebef451ce832e811a
Viktor Plattner <plattnerviktor@gmail.com>
2021-03-04 15:59:44 +0000

    plotting speed, reading data from multiple sessions

--- turning_analysis.R
+++ turning_analysis.R
@@ -1,10 +1,17 @@
 ### reading data, adding new variables ----
+scale <- 1.38 ### pixel/mm
+fps <- 30 ### frame rate
+time_res <- 1 / fps ### time between frames
 
-coords <- read.csv(file.path("turning", "coords.csv")) %>%
+coords <- read.csv(file.path("turning", "video2_coords.csv")) %>%
   as_tibble() %>%
-  mutate(rec_time = (frame / 30) %>% round(digits = 2)) %>%
-  mutate(stim = ifelse(
-    frame >= 1159 & frame <= 1307 | frame >= 1798 & frame <= 2094,
+  dplyr::mutate(rec_time = (frame / fps) %>% round(digits = 2)) %>%
+  dplyr::mutate(stim = ifelse(
+    frame >= 8359 & frame <= 8609 |
+      frame >= 1869  & frame <= 2119 |
+      frame >= 3625 & frame <= 3875 |
+      frame >= 4914 & frame <= 5164 |
+      frame >= 6319 & frame <= 6569,
     yes = T, no = F
   )) %>%
   unite(col = "head", head1, head2, sep = "_") %>%
@@ -15,30 +22,86 @@ coords <- read.csv(file.path("turning", "coords.csv")) %>%
     value = "coordinate",
     head, center, tail
   ) %>%
-  extract(col = coordinate, into = c("X", "Y"), regex = "([[:alnum:]]+)_([[:alnum:]]+)") %>%
-  mutate(X = as.integer(X), Y = as.integer(Y)) %>%
+  tidyr::extract(col = coordinate, into = c("X", "Y"), regex = "([[:alnum:]]+)_([[:alnum:]]+)") %>%
+  dplyr::mutate(X = as.integer(X), Y = as.integer(Y)) %>%
   dplyr::group_by(body_part) %>%
   mutate(
-    X_diff = diff(X) %>% c(NA),
-    Y_diff = diff(Y) %>% c(NA)
+    X_diff = c(diff(X), NA),
+    Y_diff = c(diff(Y), NA)
   ) %>%
-  mutate(d = sqrt(X_diff^2 + Y_diff^2)) %>%
-  mutate(speed = d / frame,
-         sum_d = ifelse(rec_time %% 1 == 0, cumsum(d), NA),
-         cumsumd = cumsum(d),
-         sec = cumsum (ifelse(rec_time %% 1 == 0, 1, 0))
-         ) %>% 
-  group_by(sec, body_part) %>% 
-  mutate(sum_d2 = sum(d))
-  # ungroup() %>% 
-  # mutate(sum_d = NA) %>% 
-  # dplyr::group_by(body_part) %>% 
-  # dplyr::mutate(sum_d = replace(sum_d, list = (rec_time %% 1 == 0), values = rowSums(d, 30)))
+  dplyr::mutate(
+    X_diff = ifelse(abs(X_diff) > 30, NA, X_diff),
+    Y_diff = ifelse(abs(Y_diff) > 30, NA, Y_diff)
+  ) %>%
+  dplyr::mutate(
+    d = sqrt(X_diff^2 + Y_diff^2),
+    d_mm = d / scale
+  ) %>%
+  dplyr::mutate(
+    speed_instant = d_mm / time_res,
+    mmps = speed_instant * (1 / time_res),
+    mps = (speed_instant * (1 / time_res)) / 1000,
+    # sum_d_mm = ifelse(rec_time %% 1 == 0, cumsum(d_mm), NA),
+    # cumsumd = cumsum(d_mm),
+    sec = cumsum(ifelse(rec_time %% 1 == 0, 1, 0))
+  ) %>%
+  group_by(sec, body_part) %>%
+  dplyr::mutate(speed_sec = sum(d_mm, na.rm = T)) %>%
+  ungroup() %>% 
+  group_by(stim) %>% 
+  dplyr::mutate(mean_mps_roll = zoo::rollmean(x = mps, k = 60,fill = NA))
+# ungroup() %>%
+# mutate(sum_d = NA) %>%
+# dplyr::group_by(body_part) %>%
+# dplyr::mutate(sum_d = replace(sum_d, list = (rec_time %% 1 == 0), values = rowSums(d, 30)))
+
+
+coords %>%
+  dplyr::filter(body_part == "center") %>%
+  # dplyr::slice(1:200) %>%
+  ggplot(
+    mapping = aes(x = X, y = Y, label = frame)
+  ) +
+  geom_point(aes(shape = body_part), size = 2) +
+  geom_line(aes(group = frame, col = stim)) +
+  scale_color_manual(
+    values = c("black", cols[2]),
+    name = "Stimulus",
+    labels = c("Off", "On")
+  ) +
+  scale_shape(
+    name = "Body part",
+    labels = c("Center", "Head", "Tail")
+  ) +
+  xlim(c(0,1280)) +
+  ylim(c(0,720))
+#  geom_label_repel()
+
+
+
+
+coords %>%
+  dplyr::filter(body_part == "center") %>%
+  ggplot(mapping = aes(x = Y_diff)) +
+  geom_histogram()
+
+coords %>%
+  dplyr::filter(body_part == "center") %>%
+  # dplyr::summarise(mean_mps = mean(mps, na.rm = T))
+  # dplyr::mutate(d_mm = ifelse(d_mm > 30, NA, d_mm)) %>%
+  # dplyr::mutate(speed_instant = ifelse(speed_instant > 1000, NA, speed_instant)) %>%
+  ggplot() +
+  geom_point(mapping = aes(x = rec_time, y = mean_mps_roll, color = stim, group = stim)) + 
+  geom_line(mapping = aes(x = rec_time, y = mps, color = stim, group = stim))
   
+# geom_smooth(method = "gam")
+# geom_line(mapping = aes(x = rec_time, y = speed))
+
+
 
 
 library(RcppRoll)
- 
+
 coords[!duplicated(coords$body_part), ]
 
 coords %>%
@@ -76,7 +139,9 @@ p <- coords %>%
   scale_shape(
     name = "Body part",
     labels = c("Center", "Head", "Tail")
-  )
+  ) +
+  xlim(c(0,1280)) +
+  ylim(c(0,720))
 p
 
 anim <- p +
@@ -87,7 +152,7 @@ anim <- p +
 
 ### saving animation
 gganimate::anim_save(
-  "mouse_trajec",
+  "mouse_trajec_4.gif",
   animation = gganimate::animate(anim, height = 800, width = 800, fps = 5)
 )
 
@@ -120,8 +185,8 @@ coords %>%
 
 coords %>%
   dplyr::filter(body_part != "tail") %>%
-  #dplyr::group_by(body_part) %>%
-  #slice(1:3) %>%
+  # dplyr::group_by(body_part) %>%
+  # slice(1:3) %>%
   ### each frame is present twice (for each body part)
   dplyr::group_by(frame) %>%
   summarise(
@@ -143,20 +208,20 @@ coords %>%
       y = c(leadx, leady),
       degree = T
     ) %>% as.vector(),
-    signed_angle = (atan2(leadx, leady) - atan2(vectx, vecty)) * 180 / (pi)   
+    signed_angle = (atan2(leadx, leady) - atan2(vectx, vecty)) * 180 / (pi)
     # angleabs = matlib::angle(
     #   x = c(vectxabs, vectyabs),
     #   y = c(leadxabs, leadyabs),
     #   degree = T) %>% as.vector()
-  ) %>% 
+  ) %>%
   add_column(
     rec_time = coords %>% dplyr::filter(body_part == "head") %>% pull(rec_time),
     stim = coords %>% dplyr::filter(body_part == "head") %>% pull(stim)
-  ) %>% 
-  ungroup() %>% 
-  dplyr::group_by(stim) %>% 
-  mutate(sum_angle = cumsum(signed_angle)) 
-  dplyr::group_by(stim) %>% 
+  ) %>%
+  ungroup() %>%
+  dplyr::group_by(stim) %>%
+  mutate(sum_angle = cumsum(signed_angle))
+dplyr::group_by(stim) %>%
   summarise(sum(signed_angle, na.rm = T))
 
 
