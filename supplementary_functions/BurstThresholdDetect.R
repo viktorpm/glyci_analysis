d601a6ded8ca436db454fa7a0e69fa70f4bf3887
viktorpm <plattnerviktor@gmail.com>
2021-12-08 17:27:54 +0000

    defining 3 different burst tresholds, see description in the code

--- supplementary_functions/BurstThresholdDetect.R
+++ supplementary_functions/BurstThresholdDetect.R
@@ -46,19 +46,45 @@ BurstThresholdDetect <- function(hist_data, histbreaks, isi_range) {
     clustered_d_log = F
   }
   
+
+  
+  plot(log(density$y), type = "l")
+  
+  ### half way between the max value of the first and second half of the ISI window (range)
+  index_treshold1 <- ((max2 - max1) / 2) + max1
+  
+  ### minimal value between max1 and max2
+  index_treshold2 <- which(isihist_threshold$counts[c(max1:max2)] %>% min() == isihist_threshold$counts[c(max1:max2)]) + (max1-1)
+  
+  
+  ### minimum value between threshold1 and max1. This is the best cluster threshold
+  index_threshold3 <- which(isihist_threshold$counts[c(max1:index_treshold1)] %>% min() == isihist_threshold$counts[c(max1:index_treshold1)]) + (max1 - 1)
+  
+  
+  burst_threshold <- isihist_threshold$mids[index_treshold1] 
+  burst_threshold2 <- isihist_threshold$mids[index_treshold2] 
+  burst_threshold3 <- isihist_threshold$mids[index_threshold3]
   
   hist(hist_data_filt, breaks = histbreaks, xlim = isi_range)
   abline(v = isihist_threshold$mids[max1])
   abline(v = isihist_threshold$mids[max2])
-  abline(v = isihist_threshold$mids[((max2 - max1) / 2) + max1], col = "red")
+  abline(v = isihist_threshold$mids[index_treshold1], col = "red")
+  abline(v = isihist_threshold$mids[index_treshold2], col = "magenta")
+  abline(v =isihist_threshold$mids[index_threshold3], col = "blue")
   
-  plot(log(density$y), type = "l")
+  # browser()
+  if (isihist_threshold$counts[index_threshold3] < isihist_threshold$counts[max2]) {
+    clustered = T
+  } else {clustered = F}
   
-  burst_threshold <- isihist_threshold$mids[((max2 - max1) / 2) + max1]
-  return(list(bt = burst_threshold, 
-              p_val_multipeak = diptest$p.value,
-              p_val_density_log = diptest_density_log$p.value,
-              clustered = clustered %>% as.character(),
-              clustered_d_log = clustered_d_log %>% as.character(),
+  # browser()
+  return(list(bt = burst_threshold,
+              bt2 = burst_threshold2,
+              bt3 = burst_threshold3,
+              # p_val_multipeak = diptest$p.value,
+              # p_val_density_log = diptest_density_log$p.value,
+              # clustered = clustered %>% as.character(),
+              # clustered_d_log = clustered_d_log %>% as.character(),
+              clustered = clustered, 
               bins_n = isihist_threshold$breaks %>% length()))
 }
